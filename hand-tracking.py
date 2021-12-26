43a66b5fc64027c320d2bc84a378f3574bc17894
Manav Grover <77580613+racerninja@users.noreply.github.com>
2021-12-26 22:16:57 +0530

    Create hand-tracking.py

---
+++ hand-tracking.py
@@ -0,0 +1,33 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h,w,c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                if id == 4:
+                    cv2.circle(img,(cx,cy),15,(56, 217, 28), cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (30,50), cv2.FONT_ITALIC, 1, (128, 64, 45), 3 )
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
