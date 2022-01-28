cda89a20b0be592c76e75e6a5c9c0d509b2248ee
Manav Grover <77580613+racerninja@users.noreply.github.com>
2022-01-28 21:55:48 +0530

    Update stone-paper-scissors.py

--- stone-paper-scissors.py
+++ stone-paper-scissors.py
@@ -152,7 +152,5 @@ while True:
     fps = 1/(cTime-pTime)
     pTime = cTime
 
-    # cv2.putText(img, str(int(fps)), (30,50), cv2.FONT_HERSHEY_PLAIN, 3, (0,255,0), 3 )
-
     cv2.imshow("Image", img)
     cv2.waitKey(1)
